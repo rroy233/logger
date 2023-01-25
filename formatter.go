f22209c7cfa09071be04a8366bde65482b90b87e
Roy <oisroy233@gmail.com>
2023-01-25 12:30:46 +0800

    去掉多余的换行符

--- formatter.go
+++ formatter.go
@@ -7,12 +7,17 @@ import (
 )
 
 func (f *defaultFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	msg := entry.Message
+	if strings.HasSuffix(msg, "\n") == true {
+		msg = msg[:len(msg)-1]
+	}
+
 	data := fmt.Sprintf(
 		"%s %s [%s] %s\n",
 		strings.ToUpper(entry.Level.String()),
 		entry.Time.Format("2006-01-02 15:04:05"),
 		entry.Data["Caller"],
-		entry.Message,
+		msg,
 	)
 	return []byte(data), nil
 }
